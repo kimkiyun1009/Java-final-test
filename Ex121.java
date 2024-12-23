cd25dbb52ed7cd7bf8d156061a10d0e430f94687
kimkiyun1009 <kke9371@gmail.com>
2024-12-24 01:39:46 +0900

    ㄴ

---
+++ Ex121.java
@@ -0,0 +1,14 @@
+package kky;
+
+public class Ex121 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕!");
+		System.out.println("안녕" + "!"); //2개의 문자 출력
+		
+		String hello = "안녕!";
+		System.out.println(hello); //문자열을 hello 변수에 대입
+	}	
+
+}
